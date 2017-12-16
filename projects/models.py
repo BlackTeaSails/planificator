a6ee4bfa378cc85857ada37389b9a5a06a6a39a8
Nikita Panteleev <panteleevniki@gmail.com>
2017-12-16 19:25:12 +0100

    Modelos y migración de modelos añadidos

--- projects/models.py
+++ projects/models.py
@@ -1,1 +1,25 @@
 from django.db import models
+from django.utils import timezone
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    owner = models.ForeignKey('auth.User')
+    creation_date = models.DateTimeField(default=timezone.now)
+
+    stakeholders = models.ManyToManyField('clients.Client')
+    owner = models.ForeignKey('auth.User')
+
+    def __str__(self):
+        return self.name
+
+class Requirement(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    project = models.ForeignKey('Project')
+    models.ManyToManyField('clients.Client', through='Assessment')
+
+class Assessment(models.Model):
+    client = models.ForeignKey('clients.Client', on_delete=models.CASCADE)
+    requirement = models.ForeignKey(Requirement, on_delete=models.CASCADE)
+    value = models.IntegerField()
