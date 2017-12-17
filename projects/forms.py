6e1f0ba7eb089b460a40a9519cc801304c216f1c
rorik <np258@inlumine.ual.es>
2017-12-17 20:43:17 +0100

    form para requisitos añadido

--- projects/forms.py
+++ projects/forms.py
@@ -1,11 +1,19 @@
 from django import forms
-from .models import Project
+from .models import Project, Requirement
 from clients.models import Client
 
 class NewProjectForm(forms.ModelForm):
-    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Name'}))
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'name'}))
     description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder':'description'}))
 
     class Meta:
         model = Project
         fields = ('name', 'description', 'stakeholders')
+
+class NewRequirementForm(forms.ModelForm):
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'name'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder':'description'}))
+
+    class Meta:
+        model = Requirement
+        fields = ('name', 'description')
