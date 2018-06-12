5af0dab848ad6e2c6dce0baf77be213ebedde169
Nikita Panteleev <panteleevniki@gmail.com>
2018-06-12 23:02:03 +0200

    Update forms.py
    todo del formulario para seleccionar los clientes de un proyecto nuevo, añadido.

--- projects/forms.py
+++ projects/forms.py
@@ -5,7 +5,8 @@ from clients.models import Client
 class NewProjectForm(forms.ModelForm):
     name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'name'}))
     description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder':'description'}))
-
+    # TODO: stakeholders = FilteredSelectMultiple
+    
     class Meta:
         model = Project
         fields = ('name', 'description', 'stakeholders')
